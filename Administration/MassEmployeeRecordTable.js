d3d5bf26f4109eaa6ed632b7144928e8ebfc9211
Jordy Portuguez <jordyportuguez@gmail.com>
2025-03-18 18:33:50 -0500

    actualizar MassEmployeeRecordTable.js para envio de los registros de la tabla empleados a  la API funcion flecha recursiva

--- Administration/MassEmployeeRecordTable.js
+++ Administration/MassEmployeeRecordTable.js
@@ -27,3 +27,98 @@ document.getElementById('employeeTable').addEventListener('paste', function(even
         }
     });
 });
+
+document.getElementById('FormularioEmpleado').addEventListener('submit',  async (evento) => {
+    evento.preventDefault();
+    
+        const USER_CREDENTIALS = {
+        CompanyDB: "SBO_ZICSA_05122024",
+        UserName: document.getElementById("inputUserCode").value.trim(),
+        Password: document.getElementById("inputPassword").value.trim(),  
+};
+   
+try{
+const response= await fetch("https://192.168.1.10:50000/b1s/v1/Login", {
+  method: 'POST',
+  mode: "cors",
+  credentials: 'include',
+  headers: {
+    'Content-Type': 'application/json'   
+         },
+body: JSON.stringify(USER_CREDENTIALS)
+   });
+   if (response.ok || response.status==401) {
+    const data = await response.json();
+    // Verificar si el login es exitoso capturara un SessionId
+    if (data.SessionId) {
+        spinner.style.display = 'none';
+        window.location.href ="/index.html"; // Redirige al index.html
+        console.log(data);
+    } else {
+        spinner.style.display = 'none';
+        alert('Credenciales inválidas, por favor intenta nuevamente.');
+        console.log(data.error.message.value);
+    }
+} else {
+      spinner.style.display = 'none';
+      alert('Error en la comunicación con el servidor.');
+}
+}
+catch (error) {
+        spinner.style.display = 'none';
+        console.error('Error:', error);
+        alert('Ocurrió un error inesperado en la aplicación. Vuelta a Intentar.');
+}
+
+});
+
+
+    document.getElementById('createEmployee').addEventListener('click', function(event) {
+    event.preventDefault();
+
+    const table = document.getElementById('employeeTable');
+    const rows = table.getElementsByTagName('tbody')[0].getElementsByTagName('tr');
+
+    const sendEmployee = (rowIndex) => {
+        if (rowIndex >= rows.length) {
+            return; // Terminar si se han procesado todas las filas
+        }
+
+        const cells = rows[rowIndex].getElementsByTagName('td');
+        if (cells.length > 0) {
+            const employee = {
+                "OfficeExtension" : cells[1].textContent,
+                "FirstName" : cells[2].textContent,
+                "MiddleName": cells[3].textContent,
+                "LastName": cells[4].textContent
+            };
+
+            fetch('https://192.168.1.10:50000/b1s/v1/EmployeesInfo', {
+                method: 'POST',
+                credentials: 'include',
+                mode: "cors",
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(employee)
+            })
+            .then(response => response.json())
+            .then(data => {
+                const resultCell = rows[rowIndex].getElementsByTagName('td')[5];
+                resultCell.textContent = "Empleado Creado : "+data.EmployeeID;
+                sendEmployee(rowIndex + 1); // Procesar la siguiente fila
+                console.log("Empleado Creado : "+data.EmployeeID);
+            })
+            .catch(error => {
+                const resultCell = rows[rowIndex].getElementsByTagName('td')[5];
+                resultCell.textContent = 'Error: ' + error.message;
+                sendEmployee(rowIndex + 1); // Procesar la siguiente fila a pesar del error
+                console.log(data.error.message.value);
+            });
+        } else {
+             sendEmployee(rowIndex + 1); // Procesar la siguiente fila si la actual esta vacia
+        }
+    };
+
+    sendEmployee(0); // Iniciar el procesamiento desde la primera fila
+});
