da49ed1e172651d50ab8d32e6e13c27310ccc40c
Jordy Portuguez <jordyportuguez@gmail.com>
2025-03-24 11:45:57 -0500

    Agregar spinner Cargando... en MassEmployeeRecordTable css js html, borrar codigo que se uso de ejemplo en MassEmployeeRecordTable.js

--- Administration/MassEmployeeRecordTable.js
+++ Administration/MassEmployeeRecordTable.js
@@ -1,3 +1,5 @@
+const spinner = document.getElementById('loadingContainer');
+
 document.getElementById('employeeTable').addEventListener('paste', function(event) {
     event.preventDefault();
     const text = (event.clipboardData || window.clipboardData).getData('text');
@@ -28,59 +30,15 @@ document.getElementById('employeeTable').addEventListener('paste', function(even
     });
 });
 
-document.getElementById('FormularioEmpleado').addEventListener('submit',  async (evento) => {
-    evento.preventDefault();
-    
-        const USER_CREDENTIALS = {
-        CompanyDB: "SBO_ZICSA_05122024",
-        UserName: document.getElementById("inputUserCode").value.trim(),
-        Password: document.getElementById("inputPassword").value.trim(),  
-};
-   
-try{
-const response= await fetch("https://192.168.1.10:50000/b1s/v1/Login", {
-  method: 'POST',
-  mode: "cors",
-  credentials: 'include',
-  headers: {
-    'Content-Type': 'application/json'   
-         },
-body: JSON.stringify(USER_CREDENTIALS)
-   });
-   if (response.ok || response.status==401) {
-    const data = await response.json();
-    // Verificar si el login es exitoso capturara un SessionId
-    if (data.SessionId) {
-        spinner.style.display = 'none';
-        window.location.href ="/index.html"; // Redirige al index.html
-        console.log(data);
-    } else {
-        spinner.style.display = 'none';
-        alert('Credenciales inválidas, por favor intenta nuevamente.');
-        console.log(data.error.message.value);
-    }
-} else {
-      spinner.style.display = 'none';
-      alert('Error en la comunicación con el servidor.');
-}
-}
-catch (error) {
-        spinner.style.display = 'none';
-        console.error('Error:', error);
-        alert('Ocurrió un error inesperado en la aplicación. Vuelta a Intentar.');
-}
-
-});
-
-
     document.getElementById('createEmployee').addEventListener('click', function(event) {
     event.preventDefault();
-
+    spinner.style.display = 'flex'; // Mostrar spinner    
     const table = document.getElementById('employeeTable');
     const rows = table.getElementsByTagName('tbody')[0].getElementsByTagName('tr');
 
     const sendEmployee = (rowIndex) => {
-        if (rowIndex >= rows.length) {
+        if (rowIndex >= rows.length) {          
+            spinner.style.display = 'none';
             return; // Terminar si se han procesado todas las filas
         }
 
