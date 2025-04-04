157111d3127691d92da3d789fe6e448bfc09b14e
Jordy Portuguez <jordyportuguez@gmail.com>
2025-04-04 11:38:04 -0500

    Agregar creación de empleados masivos usando hojas de calculo en archivo EmployeeSpreadSheetImport.js  se replico de MassEmployeeRecordTable.js tambien se replico el css del spinner

--- Administration/EmployeeSpreadsheetImport.js
+++ Administration/EmployeeSpreadsheetImport.js
@@ -1,3 +1,5 @@
+const spinner = document.getElementById('loadingContainer');
+
 document.addEventListener('DOMContentLoaded', () => {
     const importButton = document.getElementById('ImportButton');
     const fileInput = document.getElementById('formFile');
@@ -52,4 +54,66 @@ document.addEventListener('DOMContentLoaded', () => {
             }
         });
     }
+});
+
+
+document.getElementById('createEmployee').addEventListener('click', function(event) {
+    event.preventDefault();
+    spinner.style.display = 'flex'; // Mostrar spinner    
+    const table = document.getElementById('employeeTable');
+    const rows = table.getElementsByTagName('tbody')[0].getElementsByTagName('tr');
+
+    const sendEmployee = (rowIndex) => {
+        if (rowIndex >= rows.length) {          
+            spinner.style.display = 'none';
+            return; // Terminar si se han procesado todas las filas
+        }
+
+        const cells = rows[rowIndex].getElementsByTagName('td');
+        if (cells.length > 0) {
+            const employee = {
+                "OfficeExtension" : cells[1].textContent,
+                "FirstName" : cells[2].textContent,
+                "MiddleName": cells[3].textContent,
+                "LastName": cells[4].textContent
+            };
+
+            fetch('https://192.168.1.10:50000/b1s/v1/EmployeesInfo', {
+                method: 'POST',
+                credentials: 'include',
+                mode: "cors",
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(employee)
+            })
+            .then(response => response.json())
+            .then(data => {
+                const resultCell = rows[rowIndex].getElementsByTagName('td')[5];
+                if(data.EmployeeID)
+                {
+                    resultCell.textContent = "Empleado Creado : "+data.EmployeeID;
+                    console.log("Empleado Creado : "+data.EmployeeID);
+                }    
+                else
+                {
+                    resultCell.textContent = data.error.message.value;
+                    console.log(resultCell.textContent );
+
+                }
+                sendEmployee(rowIndex + 1); // Procesar la siguiente fila
+                
+            })
+            .catch(error => {
+                const resultCell = rows[rowIndex].getElementsByTagName('td')[5];
+                resultCell.textContent = 'Error: ' + error.message;
+                sendEmployee(rowIndex + 1); // Procesar la siguiente fila a pesar del error
+                console.log(data.error.message.value);
+            });
+        } else {
+             sendEmployee(rowIndex + 1); // Procesar la siguiente fila si la actual esta vacia
+        }
+    };
+
+    sendEmployee(0); // Iniciar el procesamiento desde la primera fila
 });
